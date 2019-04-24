df421748fb7556e7f550287b2a46fde2e491f5a6
Ace Young <ace.young289@gmail.com>
2019-04-24 16:56:58 -0400

    Add some copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>Hello, my name is Ace Young</h1>
+
+    <small>Pronouns: she/her or they/them.<br/>Fun fact: I don’t have a gender.</small>
+
+    <p>
+      I’m a front-end developer at Tank Design. I love writing weird CSS and
+      even weirder JavaScript. Not incomprehensibly weird, or utterly
+      unextendable by others. Just mildly funky code.
+    </p>
+
+    <p>
+      These days, I’m trying to learn more about design systems. Also how to
+      maintain a bicycle.
+    </p>
+
+    <p>
+      I used to have a very pretty website with lots of artwork. One day all
+      those dead paintings will be moved here.
+    </p>
+
+    <h3>Contact:</h3>
+    <ul>
+      <li>ace.young289@gmail.com</li>
+      <li>
+        <a href="https://github.com/aceyoung9">Github: aceyoung9</a>
+      </li>
+      <li>
+        I am on Twitter, but I probably won’t add you unless we’re friends in
+        real life.
+      </li>
+    </ul>
+  </div>
+)
